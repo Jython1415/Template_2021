e12dfe80eae7b035818d30dc43d8e66232760342
Jython1415 <joshua.t.shew@gmail.com>
2021-02-26 17:20:35 +0900

    added testing pragma
    pragma for team c bot on 2/26

--- src/pragma.cpp
+++ src/pragma.cpp
@@ -7,10 +7,10 @@ vex::competition Competition;
 vex::controller Controller_1 = vex::controller();
 
 // ================ name =========== port ======== reverse = brake type = slew? = voltage? = act pwr = tar pwr = rot === d rot
-Motor left_front  ( "left front"   , vex::PORT13 , false   , 0          , false , false    , false   , true    , false , false );
-Motor left_back   ( "left back"    , vex::PORT14 , false   , 1          , false , false    , false   , true    , false , false );
-Motor right_front ( "right front"  , vex::PORT20 , true    , 2          , false , false    , false   , true    , false , false );
-Motor right_back  ( "right back"   , vex::PORT19 , true    , 3          , false , false    , false   , true    , false , false );
+Motor left_front  ( "left front"   , vex::PORT1  , true    , 0          , false , false    , false   , true    , false , false );
+Motor left_back   ( "left back"    , vex::PORT9  , false   , 1          , false , false    , false   , true    , false , false );
+Motor right_front ( "right front"  , vex::PORT5  , false   , 2          , false , false    , false   , true    , false , false );
+Motor right_back  ( "right back"   , vex::PORT8  , true    , 3          , false , false    , false   , true    , false , false );
 
 std::list<Motor> Motors = {left_front   ,
 /* ==================== */ left_back    ,
