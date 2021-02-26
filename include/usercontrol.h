edd308984503eb7ffed5808a19762cc99a1aeaaf
Jython1415 <joshua.t.shew@gmail.com>
2021-02-26 16:50:42 +0900

    updated usercontrol .ccp and .h

--- include/usercontrol.h
+++ include/usercontrol.h
@@ -7,7 +7,6 @@
 #include "motor/class.h"
 #include "motor/functions.h"
 #include "custom_math.h"
-#include "sejun_code.h"
 
 // constants
 #define TANK_DRIVE_JOYSTICK_THRESHOLD    5
@@ -15,5 +14,24 @@
 
 // simple tank ctrl w/ threshold ==================================================================
 void user_tank_1 () ;
+
+// tanks ctrl w/ threshold and fast/slow ==========================================================
+#define user_tank_2_fast_multiplier 1.0
+#define user_tank_2_slow_multiplier 0.6
+#define user_tank_2_turn_multiplier 1.0
+
+extern int user_tank_2_mode                ; // 0 is slow, 1 is fast
+extern double user_tank_2_speed_multiplier ;
+
+void user_tank_2 () ;
+
+// arcade ctrl w/ threshold and fast/slow =========================================================
+
+#define user_arcade_1_fast_multiplier 1.0
+#define user_arcade_1_slow_multiplier 0.4
+
+extern int user_arcade_1_mode ; // 0 is slow, 1 is fast
+
+void user_arcade_1 () ;
 
 #endif
--- src/usercontrol.cpp
+++ src/usercontrol.cpp
@@ -1,5 +1,4 @@
 #include "usercontrol.h"
-#include "sejun_code.h"
 
 // simple tank ctrl w/ threshold ==================================================================
 void user_tank_1 ()
@@ -16,5 +15,97 @@ void user_tank_1 ()
   if (abs(ctlr_axis2) < TANK_DRIVE_JOYSTICK_THRESHOLD)
   {
     right_set(0);
+  }
+}
+
+// tanks ctrl w/ threshold and fast/slow + slow turns  ============================================
+int user_tank_2_mode = 1; // 0 is slow, 1 is fast
+double user_tank_2_speed_multiplier; // used to keep track of how fast it should be going
+
+void user_tank_2 ()
+{
+  // change drive mode based on button input
+  if (ctlr_buttonL2)
+  {
+    user_tank_2_mode = 0;
+  }
+  else
+  {
+    user_tank_2_mode = 1;
+  }
+
+  // change drive multiplier
+  if (user_tank_2_mode)
+  {
+    user_tank_2_speed_multiplier = user_tank_2_fast_multiplier;
+  }
+  else
+  {
+    user_tank_2_speed_multiplier = user_tank_2_slow_multiplier;
+  }
+  // slow down turns
+  if (math_sgn_i(ctlr_axis2) != math_sgn_i(ctlr_axis3) && math_sgn_i(ctlr_axis2) + math_sgn_i(ctlr_axis3) == 0)
+  {
+    user_tank_2_speed_multiplier *= user_tank_2_turn_multiplier;
+  }
+
+  // set left power (and drive threshold)
+  left_set(ctlr_axis3 * user_tank_2_speed_multiplier);
+  if (abs(ctlr_axis3) < TANK_DRIVE_JOYSTICK_THRESHOLD)
+  {
+    left_set(0);
+  }
+
+  // set right power (and drive threshold)
+  right_set(ctlr_axis2 * user_tank_2_speed_multiplier);
+  if (abs(ctlr_axis2) < TANK_DRIVE_JOYSTICK_THRESHOLD)
+  {
+    right_set(0);
+  }
+}
+
+// arcade ctrl w/ threshold and fast/slow =========================================================
+
+int user_arcade_1_mode = 1;
+int user_arcade_1_l       ;
+int user_arcade_1_r       ;
+
+void user_arcade_1 ()
+{
+  if (ctlr_buttonUP_pressed)
+  {
+    user_arcade_1_mode = 1;
+  }
+  if (ctlr_buttonLEFT_pressed)
+  {
+    user_arcade_1_mode = 0;
+  }
+
+  user_arcade_1_l = math_restrict_i(ctlr_axis2 + ctlr_axis4, -100, 100);
+  if (user_arcade_1_mode)
+  {
+    user_arcade_1_l = user_arcade_1_l * user_arcade_1_fast_multiplier;
+  }
+  else
+  {
+    user_arcade_1_l = user_arcade_1_l * user_arcade_1_slow_multiplier;
+  }
+  if (abs(user_arcade_1_l) < ARCADE_DRIVE_JOYSTICK_THRESHOLD)
+  {
+    left_set(user_arcade_1_l);
+  }
+
+  user_arcade_1_r = math_restrict_i(ctlr_axis2 - ctlr_axis4, -100, 100);
+  if (user_arcade_1_mode)
+  {
+    user_arcade_1_l = user_arcade_1_r * user_arcade_1_fast_multiplier;
+  }
+  else
+  {
+    user_arcade_1_r = user_arcade_1_r * user_arcade_1_slow_multiplier;
+  }
+  if (abs(user_arcade_1_r) < ARCADE_DRIVE_JOYSTICK_THRESHOLD)
+  {
+    right_set(user_arcade_1_r);
   }
 }
