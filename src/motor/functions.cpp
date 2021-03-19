e83eb16d5c91b172b5a5339233cc4266874715e2
Jython1415 <36921818+Jython1415@users.noreply.github.com>
2021-03-19 17:28:01 +0900

    removed more code (temp)
    for build

--- src/motor/functions.cpp
+++ src/motor/functions.cpp
@@ -1,6 +1,6 @@
-// #include "vex.h"
-// #include "pragma.h"
-// #include "motor/class.h"
+#include "vex.h"
+#include "pragma.h"
+#include "motor/class.h"
 
 // // set target functions ===========================================================================
 // void left_set (int input)
@@ -26,55 +26,55 @@
 //   }
 // }
 
-// // update functions  ==============================================================================
-// // update vars
-// void motors_update_vars ()
-// {
-//   for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
-//   {
-//     it->update_vars();
-//   }
-// }
-// // update power vel
-// void motors_update_power_vel ()
-// {
-//   for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
-//   {
-//     it->update_power_vel();
-//   }
-// }
-// // update power vol
-// void motors_update_power_vol ()
-// {
-//   for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
-//   {
-//     it->update_power_vol();
-//   }
-// }
-// // update power
-// void motors_update_power ()
-// {
-//   for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
-//   {
-//     it->update_power();
-//   }
-// }
-// // update rotation vars
-// void motors_update_rotation_vars ()
-// {
-//   for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
-//   {
-//     it->update_rotation_vars();
-//   }
-// }
-// // update brake types
-// void motors_update_brake_type ()
-// {
-//   for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
-//   {
-//     it->update_brake_type();
-//   }
-// }
+// update functions  ==============================================================================
+// update vars
+void motors_update_vars ()
+{
+  for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
+  {
+    it->update_vars();
+  }
+}
+// update power vel
+void motors_update_power_vel ()
+{
+  for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
+  {
+    it->update_power_vel();
+  }
+}
+// update power vol
+void motors_update_power_vol ()
+{
+  for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
+  {
+    it->update_power_vol();
+  }
+}
+// update power
+void motors_update_power ()
+{
+  for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
+  {
+    it->update_power();
+  }
+}
+// update rotation vars
+void motors_update_rotation_vars ()
+{
+  for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
+  {
+    it->update_rotation_vars();
+  }
+}
+// update brake types
+void motors_update_brake_type ()
+{
+  for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
+  {
+    it->update_brake_type();
+  }
+}
 
 // // reset rotation =================================================================================
 // void left_rotation_reset ()
@@ -92,10 +92,10 @@
 //   left_rotation_reset();
 //   right_rotation_reset();
 // }
-// void motors_rotation_reset ()
-// {
-//   for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
-//   {
-//     it->rotation_reset();
-//   }
-// }
+void motors_rotation_reset ()
+{
+  for (std::list<Motor>::iterator it=Motors.begin(); it != Motors.end(); ++it)
+  {
+    it->rotation_reset();
+  }
+}
